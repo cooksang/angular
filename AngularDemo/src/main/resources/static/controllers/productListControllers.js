fe96b15b971aa7e33bc79df65c87bb39ecf45cd1
oh <cooksang@gmail.com>
2016-04-29 14:59:53 +0900

    dd

--- AngularDemo/src/main/resources/static/controllers/productListControllers.js
+++ AngularDemo/src/main/resources/static/controllers/productListControllers.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
 .constant("productListActiveClass", "btn-primary")
-.constant("productListPageCount", 3)
-.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount){
+.constant("productListPageCount", 1)
+.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart){
 	var selectedCategory = null;
 	
 	$scope.selectedPage = 1;
@@ -14,7 +14,7 @@ angular.module("sportsStore")
 	$scope.selectPage = function(newPage){
 		$scope.selectedPage = newPage;
 	}
-	
+	cnt = 0;
 	$scope.categoryFilterFn = function(product){
 		return selectedCategory == null || product.category == selectedCategory;
 	}
@@ -25,5 +25,9 @@ angular.module("sportsStore")
 
 	$scope.getPageClass = function (page){
 		return $scope.selectedPage == page ? productListActiveClass : "";
+	}
+	
+	$scope.addProductToCart = function (product){
+		cart.addProduct(product.id, product.name, product.price);
 	}
 });
